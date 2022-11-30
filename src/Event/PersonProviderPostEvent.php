8d145411476f522f867dbd7442f7a5fbd91e6f68
Tobias Gross-Vogt <tgros@tugraz.at>
2022-11-30 13:35:39 +0100

    person provider post event

--- src/Event/PersonProviderPostEvent.php
+++ src/Event/PersonProviderPostEvent.php
@@ -4,34 +4,9 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\BasePersonBundle\Event;
 
-use Dbp\Relay\BasePersonBundle\Entity\Person;
-use Dbp\Relay\CoreBundle\LocalData\LocalDataAwarePostEvent;
+use Dbp\Relay\CoreBundle\LocalData\LocalDataPostEvent;
 
-class PersonProviderPostEvent extends LocalDataAwarePostEvent
+class PersonProviderPostEvent extends LocalDataPostEvent
 {
     public const NAME = 'dbp.relay.base_person_bundle.person_provider.post';
-
-    /** @var array */
-    private $sourceData;
-
-    /** @var Person */
-    private $person;
-
-    public function __construct(array $sourceData, Person $person)
-    {
-        parent::__construct($person);
-
-        $this->sourceData = $sourceData;
-        $this->person = $person;
-    }
-
-    public function getEntity(): Person
-    {
-        return $this->person;
-    }
-
-    public function getSourceData(): array
-    {
-        return $this->sourceData;
-    }
 }
