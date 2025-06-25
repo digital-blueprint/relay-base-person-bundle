ddbab9179d1b58d3fa4fa5fd107cbe3704cc7380
Tobias Gross-Vogt <tobias.gross-vogt@tugraz.at>
2025-06-25 15:21:11 +0200

    Remove support for deprecate queryLocal query parameter

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,58 +2,60 @@
 
 ## Unreleased
 
+- Remove support for deprecate 'queryLocal' query parameter
+
 ## v0.2.38
 
-* OpenAPI fixes for api-platform >=4.1
+- OpenAPI fixes for api-platform >=4.1
 
 ## v0.2.37
 
-* Drop support for PHP 8.1
-* Drop support for Psalm
-* Drop support for Symfony 5
-* Add support for api-platform >=4.1 and drop for <3.4
+- Drop support for PHP 8.1
+- Drop support for Psalm
+- Drop support for Symfony 5
+- Add support for api-platform >=4.1 and drop for <3.4
 
 ## v0.2.36
 
-* Remove OpenApi example for the collection filter parameter, which makes the request fail if not removed before execution
+- Remove OpenApi example for the collection filter parameter, which makes the request fail if not removed before execution
 
 ## v0.2.35
 
-* Add public test utilities TestPersonTrait::withCurrentPerson/withPerson
+- Add public test utilities TestPersonTrait::withCurrentPerson/withPerson
 
 ## v0.2.34
 
-* Adapt to core bundle >= v0.1.190
+- Adapt to core bundle >= v0.1.190
 
 ## v0.2.33
 
-* Improve DummyPersonProvider
+- Improve DummyPersonProvider
 
 ## v0.2.31
 
-* Adjust tests for minor core bundle changes
+- Adjust tests for minor core bundle changes
 
 ## v0.2.29
 
-* Port to PHPUnit 10
-* Port from doctrine annotations to PHP attributes
+- Port to PHPUnit 10
+- Port from doctrine annotations to PHP attributes
 
 ## v0.2.28
 
-* Add support for api-platform 3.2
+- Add support for api-platform 3.2
 
 ## v0.2.26
 
-* Add support for Symfony 6
+- Add support for Symfony 6
 
 ## v0.2.24
 
-* Drop support for PHP 7.4/8.0
+- Drop support for PHP 7.4/8.0
 
 ## v0.2.23
 
-* Drop support for PHP 7.3
+- Drop support for PHP 7.3
 
 ## v0.2.19
 
-* Minor cleanups and deprecation fixes
+- Minor cleanups and deprecation fixes
--- src/DataProvider/PersonDataProvider.php
+++ src/DataProvider/PersonDataProvider.php
@@ -7,46 +7,17 @@ namespace Dbp\Relay\BasePersonBundle\DataProvider;
 use Dbp\Relay\BasePersonBundle\API\PersonProviderInterface;
 use Dbp\Relay\BasePersonBundle\Entity\Person;
 use Dbp\Relay\CoreBundle\Rest\AbstractDataProvider;
-use Dbp\Relay\CoreBundle\Rest\Options;
-use Dbp\Relay\CoreBundle\Rest\Query\Filter\FilterTreeBuilder;
-use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 
 /**
  * @extends AbstractDataProvider<Person>
  */
 class PersonDataProvider extends AbstractDataProvider
 {
-    private const LOCAL_DATA_BASE_PATH = 'localData.';
+    private const SEARCH_FILTER_NAME = 'search';
 
-    private PersonProviderInterface $personProvider;
-
-    /**
-     * @deprecated this is for backwards compatibility of the queryLocal parameter
-     *
-     * @var string[]
-     */
-    private array $definedLocalDataAttributes = [];
-
-    public function __construct(PersonProviderInterface $personProvider)
+    public function __construct(private readonly PersonProviderInterface $personProvider)
     {
         parent::__construct();
-
-        $this->personProvider = $personProvider;
-    }
-
-    public function setConfig(array $config): void
-    {
-        parent::setConfig($config);
-
-        // deprecated: this is for backwards compatibility of the queryLocal parameter
-        foreach ($config['local_data'] ?? [] as $localDataConfigEntry) {
-            $this->definedLocalDataAttributes[] = $localDataConfigEntry['local_data_attribute'];
-        }
-    }
-
-    protected function getResourceClass(): string
-    {
-        return Person::class;
     }
 
     protected function getItemById(string $id, array $filters = [], array $options = []): object
@@ -59,22 +30,11 @@ class PersonDataProvider extends AbstractDataProvider
      */
     protected function getPage(int $currentPageNumber, int $maxNumItemsPerPage, array $filters = [], array $options = []): array
     {
-        if ($search = ($filters['search'] ?? null)) {
+        if ($search = ($filters[self::SEARCH_FILTER_NAME] ?? null)) {
             $options[Person::SEARCH_PARAMETER_NAME] = $search;
         }
 
-        $queryLocalParameter = $filters['queryLocal'] ?? null;
-        if ($queryLocalParameter) {
-            $this->handleDeprecateQueryLocalParameter($options, $queryLocalParameter);
-        }
-
-        $persons = $this->personProvider->getPersons($currentPageNumber, $maxNumItemsPerPage, $options);
-
-        if ($queryLocalParameter) {
-            $persons = $this->removeEntitiesWithForbiddenLocalData($options, $persons);
-        }
-
-        return $persons;
+        return $this->personProvider->getPersons($currentPageNumber, $maxNumItemsPerPage, $options);
     }
 
     protected function isGrantedReadAccessToLocalDataAttribute(string $localDataAttributeName): bool
@@ -88,49 +48,5 @@ class PersonDataProvider extends AbstractDataProvider
         }
 
         return parent::isGrantedReadAccessToLocalDataAttribute($localDataAttributeName);
-    }
-
-    /**
-     * @throws \Exception
-     *
-     * @deprecated query parameter localQuery is deprecated since core bundle version 1.1.15. Clients should use the
-     *             'filter' query parameter introduced in version 1.1.15 instead.
-     */
-    private function handleDeprecateQueryLocalParameter(array &$options, string $queryLocalParameter): void
-    {
-        $queryLocalAttributes = [];
-        $filterTreeBuilder = FilterTreeBuilder::create();
-        foreach (explode(',', $queryLocalParameter) as $queryLocalAssignment) {
-            $parts = explode(':', $queryLocalAssignment);
-            if (count($parts) === 2) {
-                if (!in_array($parts[0], $this->definedLocalDataAttributes, true)) {
-                    throw new BadRequestException('local data attribute undefined');
-                }
-                $filterTreeBuilder->iContains(self::LOCAL_DATA_BASE_PATH.$parts[0], $parts[1]);
-                $queryLocalAttributes[] = $parts[0];
-            } else {
-                throw new BadRequestException('invalid localQuery format');
-            }
-        }
-        Options::addFilter($options, $filterTreeBuilder->createFilter());
-        $options['queryLocalAttributes'] = $queryLocalAttributes;
-    }
-
-    /**
-     * @deprecated query parameter localQuery is deprecated since core bundle version 1.1.15. Clients should use the
-     *             'filter' query parameter introduced in version 1.1.15 instead.
-     */
-    private function removeEntitiesWithForbiddenLocalData(array $options, array $persons): array
-    {
-        foreach ($options['queryLocalAttributes'] as $queryLocalAttribute) {
-            // since this is only for backwards compatibility and there is currently no application of a local query
-            // whee the read policy depends on the person entity
-            // we base the decision only on the user for increased performance
-            if (!$this->isGrantedRole('@read-local-data:'.$queryLocalAttribute)) {
-                return [];
-            }
-        }
-
-        return $persons;
     }
 }
